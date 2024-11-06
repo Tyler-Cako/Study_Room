71cfb608caeb5ce4e95453774029b038cc0488e5
Joshua Wright <jowr8149@colorado.edu>
2024-11-06 04:20:01 -0700

    NodeJS connects to database in docker

--- ProjectSourceCode/app/index.ts
+++ ProjectSourceCode/app/index.ts
@@ -1,8 +1,19 @@
 import express from 'express';
+import db from './db';
 
-const app = express()
+const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+// testing database
+db.connect()
+  .then(obj => {
+    console.log('Database connection successful'); // you can view this message in the docker compose logs
+    obj.done(); // success, release the connection;
+  })
+  .catch(error => {
+    console.log('ERROR:', error.message || error);
+  });
 
 app.get('/', (req, res) => {
     res.send("hello world!");
