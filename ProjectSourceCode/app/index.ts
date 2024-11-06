1bc31849b087e73a88e8278c7e40afeba60571e4
Jake Tucker <tuckerjake11@gmail.com>
2024-11-05 19:40:34 -0700

    Created websocket code based on tutorial -- not working yet

--- ProjectSourceCode/app/index.ts
+++ ProjectSourceCode/app/index.ts
@@ -1,13 +1,77 @@
 import express from 'express';
+import path from 'path';
+import {Server} from 'socket.io';
+import {io as clientIo} from 'socket.io-client'
+import { formatMessage } from './util/formatDate';
+import { getActiveUser, exitRoom, newUser, getIndividualRoomUsers } from './util/userHelper';
 
-const app = express()
+const app = express();
+const server = require('http').createServer(app);
+const io = new Server(server);
 
 const PORT = process.env.PORT || 3000;
-
+app.set('views', path.join(__dirname, 'views'));
 app.get('/', (req, res) => {
-    res.send("hello world!");
+    res.redirect('/login');
 });
 
 app.listen(PORT, () => {
     console.log(`App listening on port: ${PORT}`);
+});
+
+app.get('/login', (req, res) => {
+    res.render('views/login');
+});
+
+io.on('connection', socket => {
+    socket.on('joinRoom', ({ username, room }) => {
+        const user = newUser(socket.id, username, room);
+
+        socket.join(user.room);
+
+        // General welcome
+        socket.emit('message', formatMessage("WebCage", 'Messages are limited to this room! '));
+
+        // Broadcast everytime users connects
+        socket.broadcast
+            .to(user.room)
+            .emit(
+                'message',
+                formatMessage("WebCage", `${user.username} has joined the room`)
+            );
+
+        // Current active users and room name
+        io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getIndividualRoomUsers(user.room)
+        });
+    });
+
+    // Listen for client message
+    socket.on('chatMessage', msg => {
+        const user = getActiveUser(socket.id);
+        if (user) {
+            io.to(user.room).emit('message', formatMessage(user.username, msg))
+        } else {
+            console.error('Cannot get messages from null active user!')
+        }
+    });
+
+    // Runs when client disconnects
+    socket.on('disconnect', () => {
+        const user = exitRoom(socket.id);
+
+        if (user) {
+            io.to(user.room).emit(
+                'message',
+                formatMessage("WebCage", `${user.username} has left the room`)
+            );
+
+            // Current active users and room name
+            io.to(user.room).emit('roomUsers', {
+                room: user.room,
+                users: getIndividualRoomUsers(user.room)
+            });
+        }
+    });
 });
