cb56264e7dd2ee90df96137699e01fe3b33b2304
Joshua Wright <jowr8149@colorado.edu>
2024-11-06 05:18:06 -0700

    got test API routes to work and added bcrypt

--- ProjectSourceCode/app/index.ts
+++ ProjectSourceCode/app/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import bcrypt from 'bcryptjs';
 import db from './db';
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
 
 // testing database
 db.connect()
@@ -15,8 +17,49 @@ db.connect()
     console.log('ERROR:', error.message || error);
   });
 
+// TEST API ROUTES
+
 app.get('/', (req, res) => {
     res.send("hello world!");
+});
+
+app.post('/test', function (req, res) {
+    const query =
+      'insert into student (name, email, password) values ($1, $2, $3)  returning * ;';
+    db.any(query, [
+      req.body.name,
+      req.body.email,
+      req.body.password,
+    ])
+      // if query execution succeeds
+      // send success message
+      .then(function (data) {
+        res.status(201).json({
+          status: 'success',
+          data: data,
+          message: 'data added successfully',
+        });
+      })
+      // if query execution fails
+      // send error message
+      .catch(function (err) {
+        return console.log(err);
+      });
+  });
+
+app.get('/get_students', (req, res) => {
+    var query = 'SELECT * FROM student;';
+    db.any(query)
+      .then(function (data) {
+        res.status(201).json({
+            status: 'success',
+            data: data,
+            message: 'data retrieved successfully',
+        });
+      })
+      .catch(function (error) {
+        return console.log(error);
+      });
 });
 
 app.listen(PORT, () => {
