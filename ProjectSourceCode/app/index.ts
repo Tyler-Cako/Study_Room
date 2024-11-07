12f69601af51ecefaa878f5631d0a0f41486da27
Jake Tucker <tuckerjake11@gmail.com>
2024-11-06 22:26:19 -0700

    change to js

--- ProjectSourceCode/app/index.ts
+++ ProjectSourceCode/app/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import bcrypt from 'bcryptjs';
+import db from './db';
 import path from 'path';
 import {Server} from 'socket.io';
-import {io as clientIo} from 'socket.io-client'
-import { formatMessage } from './util/formatDate';
-import { getActiveUser, exitRoom, newUser, getIndividualRoomUsers } from './util/userHelper';
+import {io as clientIo} from 'socket.io-client';
+import { formatMessage } from './public/js/formatDate';
+import { getActiveUser, exitRoom, newUser, getIndividualRoomUsers } from './public/js/userHelper';
 
 const app = express();
 const server = require('http').createServer(app);
@@ -12,7 +14,22 @@ const io = new Server(server);
 const PORT = process.env.PORT || 3000;
 app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.json());
+
+// testing database
+db.connect()
+  .then(obj => {
+    console.log('Database connection successful'); // you can view this message in the docker compose logs
+    obj.done(); // success, release the connection;
+  })
+  .catch(error => {
+    console.log('ERROR:', error.message || error);
+  });
+
+// TEST API ROUTES
+
 app.get('/', (req, res) => {
     res.redirect('/login');
 });
@@ -22,6 +39,46 @@ app.get('/login', (req, res) => {
 app.get('/chat', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'chat.html'));
 });
+
+app.post('/test', function (req, res) {
+    const query =
+      'insert into student (name, email, password) values ($1, $2, $3)  returning * ;';
+    db.any(query, [
+      req.body.name,
+      req.body.email,
+      req.body.password,
+    ])
+      // if query execution succeeds
+      // send success message
+      .then(function (data) {
+        res.status(201).json({
+          status: 'success',
+          data: data,
+          message: 'data added successfully',
+        });
+      })
+      // if query execution fails
+      // send error message
+      .catch(function (err) {
+        return console.log(err);
+      });
+  });
+
+app.get('/get_students', (req, res) => {
+    var query = 'SELECT * FROM student;';
+    db.any(query)
+      .then(function (data) {
+        res.status(201).json({
+            status: 'success',
+            data: data,
+            message: 'data retrieved successfully',
+        });
+      })
+      .catch(function (error) {
+        return console.log(error);
+      });
+});
+
 app.listen(PORT, () => {
     console.log(`App listening on port: ${PORT}`);
 });
@@ -34,6 +91,7 @@ app.listen(PORT, () => {
 
 // websocket code from tutorial, converted to TS
 io.on('connection', socket => {
+    console.log('a user connected');
     io.on('joinRoom', ({ username, room }) => {
         const user = newUser(socket.id, username, room);
 
